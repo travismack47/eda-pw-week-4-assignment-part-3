42e651dcf25d651d5984daa9496d28c28e11c579
Travis Mack <travismack47@protonmail.com>
2023-04-16 15:06:27 -0500

    correctly formatted answers

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,26 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = ['Eggs', 'Bacon', 'Cereal', 'Chicken', 'Coffee']
+
+function addItem(item) {
+    basket.push(item)
+    return true;
+}
+
+function listItems() {
+    let i = 0;
+    while (i < basket.length) {
+        console.log(basket[i]);
+        i++;
+    }
+}
+
+listItems();
+addItem('Turkey');
+console.log(basket);
+
+function empty() {
+    return basket.length = 0;
+}
+console.log(empty());
